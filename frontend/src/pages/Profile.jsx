44b80a6f2ffd7257757c6608bf9e00b24e738a72
Bulu <bulusahoo944@gmail.com>
2024-04-28 11:35:40 +0530

    show user listing functionality completed

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -29,13 +29,11 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [upadteSuccess, setUpadteSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+  const [loadingListing, setLoadingListing] = useState(false);
 
   const dispatch = useDispatch();
-
-  // console.log(error, "err user");
-  // console.log(filePerc, "filePerc");
-  // console.log(formData, "formData");
-  // console.log(fileUploadError, "fileUploadError");
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
@@ -133,6 +131,24 @@ const Profile = () => {
     }
   };
 
+  const handleShowListings = async () => {
+    setShowListingError(false);
+    setLoadingListing(true);
+    try {
+      const res = await fetch(`/api/v1/listing/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      } else {
+        setLoadingListing(false);
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  };
+  console.log(userListings);
   return (
     <div className=" p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl font-semibold text-center my-7">Profile</h1>
@@ -220,6 +236,44 @@ const Profile = () => {
       <p className=" text-green-700 mt-5">
         {upadteSuccess ? "User is updated successfully" : ""}
       </p>
+      <button
+        onClick={handleShowListings}
+        className="text-green-700 w-full font-bold text-lg"
+      >
+        {loadingListing ? "Showing Listings..." : "Show Listings"}
+      </button>
+      <p className=" text-red-700 mt-5 text-sm">
+        {showListingError ? " Something went wrong while showing lists" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className=" flex flex-col gap-4" >
+          <h1 className=" text-center text-2xl font-semibold"> Your Listing</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className=" border rounded-lg p-3 flex justify-between items-center gap-2"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt=" listing image cover"
+                  className=" h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className=" text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className=" flex flex-col items-center">
+                <button className=" text-red-700 uppercase">Delete</button>
+                <button className=" text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
