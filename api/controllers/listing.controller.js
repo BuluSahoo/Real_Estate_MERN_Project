ae634963bc0aa4eaf0652b52d62c81d112b046aa
Bulu <bulusahoo944@gmail.com>
2024-04-28 19:26:36 +0530

    complete update funtionaliy

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -23,20 +23,54 @@ export const getUserListings = async (req, res, next) => {
   }
 };
 
-export const deleteListings = async (req, res, next) =>{
+export const deleteListings = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
 
-  const listing = await Listing.findById(req.params.id)
-
-  if(!listing){
-    return next(errorHandler(401, "Listing not found!"))
+  if (!listing) {
+    return next(errorHandler(401, "Listing not found!"));
   }
-  if(req.user.id !==listing.userRef){
-    return next(errorHandler(401, "You can only delete your own listigs!"))
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listigs!"));
   }
   try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json("Listing has been deleted")
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const updateListings = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own Listings"));
+  }
+
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(404, "listing not found");
+    }
+
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, getUserListings, deleteListings } from "../controllers/listing.controller.js";
+import { createListing, getUserListings, deleteListings, updateListings, getListings } from "../controllers/listing.controller.js";
 import { veryfyToken } from "../utils/verifyUser.js";
 
 const router = express.Router()
@@ -7,7 +7,8 @@ const router = express.Router()
 router.post('/create', createListing)
 router.get('/:id', veryfyToken, getUserListings)
 router.delete('/delete/:id', veryfyToken, deleteListings)
-
+router.post('/update/:id', veryfyToken, updateListings)
+router.get('/get/:id', getListings)
 
 
 export default router
